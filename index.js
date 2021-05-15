d5fedf6d71e82c76dff7bff2885dbce821c2962a
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-15 05:31:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ Function Driver(name, age, experience){
   this.age = age;
   this.experience = experience;
 } 
+
+Function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+} 
